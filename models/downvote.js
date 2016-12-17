81c5db80fedf9c27204402065a0b7dfb983fd7be
manoj337 <manojown1@gmail.com>
2016-12-18 00:23:28 +0530

    make it better

--- models/downvote.js
+++ models/downvote.js
@@ -8,7 +8,7 @@ module.exports.downvote = function(req,res)
 		console.log(req.body.cid+" my user" + req.body.user+" "+req.body.flag);
 		var data;
 		var sql = "INSERT INTO votes (cid ,email,flag) VALUES ("+req.body.cid+",'"+req.body.user+"',"+req.body.flag+") ON DUPLICATE KEY UPDATE flag=1";
-		console.log(sql)
+		//console.log(sql)
 		 con.query(sql, function(err, result) {
 
 			if(err){
