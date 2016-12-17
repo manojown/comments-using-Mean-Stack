755c0b229334f8116a4f3568cedfb327db4f6bd2
manoj337 <manojown1@gmail.com>
2016-12-18 00:36:20 +0530

    make it better1

--- models/downvote.js
+++ models/downvote.js
@@ -5,14 +5,16 @@ var con = connection.con();
 
 module.exports.downvote = function(req,res)
 {
-		console.log(req.body.cid+" my user" + req.body.user+" "+req.body.flag);
-		var data;
-		var sql = "INSERT INTO votes (cid ,email,flag) VALUES ("+req.body.cid+",'"+req.body.user+"',"+req.body.flag+") ON DUPLICATE KEY UPDATE flag=1";
+		//console.log(req.body.cid+" my user" + req.body.user+" "+req.body.flag);
+		var datainsert = {cid:req.body.cid,email:req.body.user,flag:req.body.flag};
+		//var sql = "INSERT INTO votes (cid ,email,flag) VALUES ("+req.body.cid+",'"+req.body.user+"',"+req.body.flag+") ON DUPLICATE KEY UPDATE flag=1";
 		//console.log(sql)
-		 con.query(sql, function(err, result) {
+		var data;
+		 con.query('INSERT INTO votes SET ? ON DUPLICATE KEY UPDATE flag=1',datainsert,function(err, result) {
 
 			if(err){
-				data = "success"
+				data = "fail"
+				console.log("fail");
 				res.send(JSON.stringify(data));
 			}else{
 				res.send(JSON.stringify(data));
