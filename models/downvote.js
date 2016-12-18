0d66458edd02c6b65deacf1e37f10b0bd995dcf5
manoj337 <manojown1@gmail.com>
2016-12-18 06:10:35 +0530

    server crash again create new one

--- models/downvote.js
+++ models/downvote.js
@@ -5,18 +5,19 @@ var con = connection.con();
 
 module.exports.downvote = function(req,res)
 {
-		//  for  down vote
+		//  for  down vote check all posiblity
 		var datainsert = {cid:req.body.cid,email:req.body.user,flag:req.body.flag};
 		var data;
 
 									
 		var sql = "select * from votes where email ='"+req.body.user+"' and cid="+req.body.cid+" and flag="+req.body.flag+"";
 		console.log(sql);
-		con.query(sql, function(error,rows)
+		con.query(sql, function(error,rows)  // first check if user already upvote
 					{
 						
 						if(!rows.length>0)
-						{	
+						{	 
+							// if user request for upvote and user already downvote to that comment then update it
 							
 							 con.query('INSERT INTO votes SET ? ON DUPLICATE KEY UPDATE flag=1',datainsert,function(err, result) {
 							 		console.log(result);
@@ -30,6 +31,7 @@ module.exports.downvote = function(req,res)
 									}
 								});
 						}else{
+							// user have already upvote so delete to database as user request
 
 							var sqld="DELETE FROM votes where email='"+req.body.user+"' and cid="+req.body.cid+"";
 							con.query(sqld, function(error,rows){
--- models/upvote.js
+++ models/upvote.js
@@ -11,12 +11,12 @@ module.exports.upvote = function(req,res)
 
 		 var sql = "select * from votes where email ='"+req.body.user+"' and cid="+req.body.cid+" and flag="+req.body.flag+"";
 		console.log(sql);
-		con.query(sql, function(error,rows)
+		con.query(sql, function(error,rows)  // first check if user already upvote
 					{
 						
-						if(!rows.length>0)
+						if(!rows.length>0) 
 						{	
-							
+							// if user request for upvote and user already downvote to that comment then update it
 							 con.query('INSERT INTO votes SET ? ON DUPLICATE KEY UPDATE flag=0',datainsert,function(err, result) {
 							 		console.log(result);
 									if(err){
@@ -30,6 +30,7 @@ module.exports.upvote = function(req,res)
 								});
 						}else{
 
+							// user have already upvote so delete to database as user request
 							var sqld="DELETE FROM votes where email='"+req.body.user+"' and cid="+req.body.cid+"";
 							con.query(sqld, function(error,rows){
 								console.log(sqld);
